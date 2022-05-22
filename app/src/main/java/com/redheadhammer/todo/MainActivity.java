06071af97bc7cdf13189fa21361afb2271fa43a4
Bhavuk <bhavuksharma2202@gmail.com>
2022-05-23 00:25:16 +0530

    Added register page and layout

--- app/src/main/java/com/redheadhammer/todo/MainActivity.java
+++ app/src/main/java/com/redheadhammer/todo/MainActivity.java
@@ -2,13 +2,57 @@ package com.redheadhammer.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPref;
+    TextInputEditText username, password;
+    CheckBox autologin;
+    Button submit, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        sharedPref = getSharedPreferences("autologin", MODE_PRIVATE);
+
+        /* launch register page if 1st run of the app */
+        if (sharedPref.getBoolean("isFirstRun", true)) {
+            Intent intent = new Intent(MainActivity.this, Register.class);
+            startActivity(intent);
+        }
+
+        /* auto-login if set in past */
+        if (sharedPref.getString("login", null) != null) {
+            Intent intent = new Intent(MainActivity.this, TodoList.class);
+            startActivity(intent);
+        }
+
+        /* setting view objects */
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        autologin = findViewById(R.id.autologin);
+        submit = findViewById(R.id.submit);
+        register = findViewById(R.id.register);
+
+        /* get values from login screen */
+        String user = String.valueOf(username.getText());
+        String pass = String.valueOf(password.getText());
+
+        register.setOnClickListener(this::onClick);
+
+    }
+
+    private void onClick(View view) {
+        Toast.makeText(MainActivity.this, R.string.pending, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/redheadhammer/todo/Register.java
@@ -0,0 +1,79 @@
+package com.redheadhammer.todo;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+
+    EditText userName, password, passRepeat;
+    Button register;
+
+    SharedPreferences sharedPref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        /* getShared preferences for autoLogin and saving username and password */
+        sharedPref = getSharedPreferences("autologin", MODE_PRIVATE);
+
+        /* setting view objects from activity_register */
+        userName = this.findViewById(R.id.username);
+        password = this.findViewById(R.id.password);
+        passRepeat = this.findViewById(R.id.passwordRepeat);
+        register = this.findViewById(R.id.register);
+
+        /* onClick listener for register button */
+        register.setOnClickListener(this::onClick);
+
+    }
+
+    public void onClick(View view) {
+        /* get username, password value from view objects */
+        final String user = String.valueOf(userName.getText());
+        final String pass = String.valueOf(password.getText());
+        final String passR = String.valueOf(passRepeat.getText());
+
+        /* Dialog builder for confirming account creating */
+        if (pass.equals(passR)) alert(user, pass);
+        else {
+            /* on password field mismatch setError on */
+            passRepeat.setError("Mismatch in passwords");
+            passRepeat.setText("");
+        }
+    }
+
+    private void alert(String user, String pass) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Confirm").setMessage(R.string.dialog_register);
+
+        alert.setNegativeButton("No", (dialogInterface, i) -> {
+            userName.setText("");
+            password.setText("");
+            passRepeat.setText("");
+            dialogInterface.cancel();
+        });
+
+        alert.setPositiveButton("Yes", (dialogInterface, i) -> {
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("userName", user);
+            editor.putString("password", pass);
+            editor.putBoolean("isFirstRun", false);
+            editor.apply();
+            /* go back to login activity after creating username password */
+            Intent intent = new Intent(Register.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        alert.show();
+    }
+}
